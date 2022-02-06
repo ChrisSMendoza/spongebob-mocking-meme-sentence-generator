0a7b84ff96e4ca8e0e314c359b877e276928c412
Chris Mendoza-Alecio <chris_codes@aol.com>
2022-02-06 00:59:00 -0800

    remove dead code, make logical blocks readable

--- spongebob-mock-meme-generate.js
+++ spongebob-mock-meme-generate.js
@@ -1,13 +1,16 @@
-function flipACoin() {
-    // Magic numbers are not okay.
-    // I believe this is the ceiling value for the 
-    // random number that's generated. It's non-inclusive,
-    // only the numbers up to it, but not actually it.
-    // Example, "* 4", could output: 0, 1, 2, 3
 
-    // Hence this is 0 or 1, the odds are 50/50,
-    // like flipping a coin.
-    return Math.floor(Math.random() * 2);
+
+const textInput = document.querySelector("#textInput");
+textInput.addEventListener("input", showMockMemeText);
+
+function showMockMemeText(event) {
+    const userText = event.target.value;
+
+    const mockMemeText = spongebobMockMemeGenerate(userText);
+
+    const outputTextField = document.querySelector("#spongebob-mock-formatted-text");
+
+    outputTextField.value = mockMemeText;
 }
 
 function spongebobMockMemeGenerate(text) {
@@ -27,31 +30,12 @@ function spongebobMockMemeGenerate(text) {
     return lettersForMeme.join("");
 }
 
-function showMockMemeText(event) {
-    const userText = event.target.value;
-
-    const mockMemeText = spongebobMockMemeGenerate(userText);
-
-    const outputTextField = document.querySelector("#spongebob-mock-formatted-text");
-
-    outputTextField.value = mockMemeText;
-}
-
-const textInput = document.querySelector("#textInput");
-textInput.addEventListener("input", showMockMemeText);
-
 const copyToClipboardButton = document.querySelector("#copy-to-clipboard-button");
 copyToClipboardButton.addEventListener("click", handleOnCopyToClipboard)
 
-function handleOnCopyToClipboard(event) {
-    // Consideration: can't we just run the logic ourselves?
-    // No, because that would generate another one, so it wouldn't 
-    // match what's on the screen... Update the output text too??
-
+function handleOnCopyToClipboard() {
     // Get the meme formatted text that's shown to the user
     const { value: spongebobMockMemeText } = document.querySelector("#spongebob-mock-formatted-text");
-
-    const memeDisplayTextField = document.querySelector("#spongebob-mock-formatted-text");
 
     // Copy the text to their clipboard, user can paste it now
     navigator.clipboard.writeText(spongebobMockMemeText);
