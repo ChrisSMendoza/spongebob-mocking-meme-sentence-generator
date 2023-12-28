d74a0822453af1fc109cbcb3d90334b5755ae595
Chris Mendoza <chris_codes@aol.com>
2023-12-28 03:31:56 -0800

    add and test lowercase detection

--- src/spongebob-mock-meme-generate.test.js
+++ src/spongebob-mock-meme-generate.test.js
@@ -3,6 +3,20 @@ import test from 'node:test'
 
 import { generateSpongebobMockMemeText } from './spongebob-mock-meme-generate.js'
 
-test("Syncronous add", () => {
-    strictEqual(1, 1)
+test("Generates meme text", () => {
+    const memeText = generateSpongebobMockMemeText("this text is totally not mocking you")
+
+    strictEqual(true, !!memeText)
 })
+
+test("Lowercase letter detected", () => {
+    strictEqual(isLowerCase('a'), true)
+    strictEqual(isLowerCase('z'), true)
+
+    strictEqual(isLowerCase('A'), false)
+    strictEqual(isLowerCase('Z'), false)
+})
+
+function isLowerCase(char) {
+    return char === char.toLowerCase()
+}
