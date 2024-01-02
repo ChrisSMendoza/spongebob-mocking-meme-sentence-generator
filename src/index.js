aceac812f78326a8541ed1ec4307371bc0f04916
Chris Mendoza <chris_codes@aol.com>
2024-01-02 02:41:18 -0800

    Fix type errors (#10)
    * add optional chaining to add event listener calls since may be null
    
    * remove caching from local server so refresh loads latest js
    
    * wrap get input to throw if missing to guarantee it exists for types
    
    * add git ignore to ignore node modules
    
    * set module and resolution to nodenext so assert module is known
    
    * add node types as dev dependency so TS knows about test types

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 import { generateSpongebobMockMemeText } from "./spongebob-mock-meme-generate.js"
 
 const textInput = document.querySelector("#textInput");
-textInput.addEventListener("input", showMockMemeText);
+textInput?.addEventListener("input", showMockMemeText);
 
 function showMockMemeText(event) {
     const userText = event.target.value;
 
     const mockMemeText = generateSpongebobMockMemeText(userText);
 
-    /** @type {HTMLInputElement | null} */
-    const outputTextField = document.querySelector("#spongebob-mock-formatted-text");
+    const outputTextField = getOutputTextElementOrThrow()
 
     outputTextField.value = mockMemeText;
 }
 
 const copyToClipboardButton = document.querySelector("#copy-to-clipboard-button");
-copyToClipboardButton.addEventListener("click", handleOnCopyToClipboard)
+copyToClipboardButton?.addEventListener("click", handleOnCopyToClipboard)
 
 function handleOnCopyToClipboard() {
     // Get the meme formatted text that's shown to the user
-
-    /** @type {HTMLInputElement | null} */
-    const { value: spongebobMockMemeText } = document.querySelector("#spongebob-mock-formatted-text");
+    const { value: spongebobMockMemeText } = getOutputTextElementOrThrow()
 
     // Copy the text to their clipboard, user can paste it now
     navigator.clipboard.writeText(spongebobMockMemeText);
+}
+
+/**
+ *
+ * @returns {HTMLInputElement} Output text field
+ * @throws
+ */
+function getOutputTextElementOrThrow() {
+    /** @type {HTMLInputElement | null} */
+    const outputTextField = document.querySelector("#spongebob-mock-formatted-text");
+
+    if(outputTextField) {
+        return outputTextField;
+    }
+
+    throw Error("Output text field was not found in DOM with expected ID")
 }
