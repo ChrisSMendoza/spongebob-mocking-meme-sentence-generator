ec472ab886d6ad2adb7a7847b6d1324a29d512ba
Chris Mendoza <chris_codes@aol.com>
2024-01-02 01:07:22 -0800

    Add jsconfig to set target and enforce JSDoc types and null values (#8)
    * add jsconfig to ask for JS checks to enforce JSDoc types
    
    * set target to ES2015 to allow string to be spread in array
    
    * type params and return value of generator with JSDOC
    
    * cast elements to inputs with inline type JSDOC comment
    
    * type uppercase randomly helper with JSDoc generated comment
    
    * add strict null checks
    
    * type output text field input or null

--- src/spongebob-mock-meme-generate.js
+++ src/spongebob-mock-meme-generate.js
@@ -1,6 +1,13 @@
 // Favor lowercase letters by default since we think it looks better
 export const UPPERCASE_ODDS_DEFAULT = .30
 
+/**
+ * 
+ * @param {string} text - Text to meme-ify
+ * @param {number} [uppercaseOddsFromUser] - Odds of a letter becoming uppercase
+ * 
+ * @returns {string} Text in the Spongebob mocking meme format: toTalLy LiKe tHiS
+ */
 export function generateSpongebobMockMemeText(text, uppercaseOddsFromUser) {
     const uppercaseOdds = uppercaseOddsFromUser ?? UPPERCASE_ODDS_DEFAULT;
 
@@ -10,6 +17,10 @@ export function generateSpongebobMockMemeText(text, uppercaseOddsFromUser) {
     return lettersForMeme.join("");
 }
 
+/**
+ * @param {string} text
+ * @param {number} uppercaseOdds
+ */
 function uppercaseOrLowercaseRandomly(text, uppercaseOdds) {
 
     if (Math.random() <= uppercaseOdds) {
