688b28065d54f9f024d3ab24b8e273c9228a7791
Chris Mendoza <chris_codes@aol.com>
2024-01-02 00:39:24 -0800

    type params and return value of generator with JSDOC

--- src/spongebob-mock-meme-generate.js
+++ src/spongebob-mock-meme-generate.js
@@ -1,6 +1,13 @@
 // Favor lowercase letters by default since we think it looks better
 export const UPPERCASE_ODDS_DEFAULT = .30
 
+/**
+ * 
+ * @param {string} text - Text to meme-ify
+ * @param {number} [uppercaseOddsFromUser] - Odds of a letter becoming uppercase
+ * 
+ * @returns {string} Text in the Spongebob mocking meme format: toTalLy LiKe tHiS
+ */
 export function generateSpongebobMockMemeText(text, uppercaseOddsFromUser) {
     const uppercaseOdds = uppercaseOddsFromUser ?? UPPERCASE_ODDS_DEFAULT;
 
